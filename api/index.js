39e08be89222c47e35bbb7adf1063bce4492ff98
Kyaw Thet Khaing <kyawthet.khaing@yomabank.com>
2021-08-28 21:47:14 +0630

    blog app form validation added

---
+++ api/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const multer = require("multer");
+const path = require("path");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const postsRoute = require("./routes/posts");
+const categoriesRoute = require("./routes/categories");
+
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  }
+});
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/posts", postsRoute);
+app.use("/api/categories", categoriesRoute);
+
+app.listen("5000", () => {
+  console.log("ktk-blog backend is running");
+});
